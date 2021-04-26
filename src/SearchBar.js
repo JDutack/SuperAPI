442a1b5ae3a54d0b679e1ae23536301b39a1926c
Jota <j.dutack@gmail.com>
2021-04-26 19:31:47 -0300

    escribiendo logica search

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SearchBar(props) {
+    const [searchText, setSearchText] = useState("");
+
+    async function searchSuperHeroes () {
+       const response = await fetch(`https://www.superheroapi.com/api.php/10224302364615758/search/${searchText}`);
+       const data = await response.json();
+    }
+
+    function handleChange (e) {
+        const searchTerm = e.target.value;
+
+        setSearchText(searchTerm) {
+            if (searchTerm > 3) {
+                searchSuperHeroes();
+            }
+        }
+
+    }
+
     return (
         <div id="search-main">
-            <input id="search-input" type="search" placeholder="Buscá tu súper..." />
+            <input id="search-input"
+            type="search"
+            placeholder="Buscá tu súper..."
+            onChange={handleChange}
+            />
         </div>
     );
 }
