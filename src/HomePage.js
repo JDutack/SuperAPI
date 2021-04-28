7ba70ba4847634fe0134cd1828a8a15436b8d6c3
Jota <j.dutack@gmail.com>
2021-04-28 12:23:13 -0300

    buscador finalizado

--- src/HomePage.js
+++ src/HomePage.js
@@ -6,16 +6,12 @@ function HomePage(props) {
 
     return (
         <div id="home-main">
-            <div id="team-cards">
-                <div id="result-profile-pic"><img src={data.image.url} alt="imagen-super" height="275px"/></div>
-                <div id="result-profile-data">
-                    <h2 id="card-name">{data.name}</h2>
-                    <div className="powerstats">Intelligence: {data.powerstats.intelligence}</div>
-                    <div className="powerstats">Strenght: {data.powerstats.strength}</div>
-                    <div className="powerstats">Speed: {data.powerstats.speed}</div>
-                    <div className="powerstats">Durability: {data.powerstats.durability}</div>
-                    <div className="powerstats">Power: {data.powerstats.power}</div>
-                    <div className="powerstats">Combat: {data.powerstats.combat}</div>
+            <div id="home-super-list">
+                <div id="team-cards">
+                    <div id="result-profile-pic"><img src={data.image.url} alt="imagen-super" height="275px"/></div>
+                    <div id="result-profile-data">
+                        <h2 id="card-name">{data.name}</h2>
+                    </div>
                 </div>
             </div>                        
         </div>
--- src/ResultCards.js
+++ src/ResultCards.js
@@ -9,7 +9,9 @@ function ResultCards(props) {
                 <div id="result-profile-pic"><img src={data.image.url} alt="imagen-super" height="275px"/></div>
                 <div id="result-profile-data">
                     <h1 id="card-name">{data.name}</h1>
-                    <h2 id="card-fullname">{data["full-name"]}</h2>
+                    <h2 id="card-fullname">{data.biography["full-name"]}</h2>
+                    <button id="add-button">Agregar al equipo</button>
+                    <span id="alignment">Alineación: {data.biography.alignment}</span>
                 </div>
             </div>
         </div>
